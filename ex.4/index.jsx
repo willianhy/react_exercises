37ec865be6ca90df07773fab337fcf3316b836c6
Willian Yassunaka <willianhy@gmail.com>
2017-10-25 22:57:04 -0200

    nested components

--- ex.4/index.jsx
+++ ex.4/index.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import SilvaFamily from './silvaFamily'
+import Primeiro, { Segundo } from './component'
 
 ReactDOM.render(
-    <SilvaFamily />
+<div>
+    <Primeiro />
+    <Segundo />
+</div>
 , document.getElementById('app'))
