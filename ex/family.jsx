cd5479047a2ba45c33aeecd937d64d8d3dcf0a67
Willian Yassunaka <willianhy@gmail.com>
2017-10-26 22:13:36 -0200

    passing properties to children components

--- ex/family.jsx
+++ ex/family.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import { childrenWithProps } from '../utils/reactUtils'
 
 export default props => (
     <div>
         <h1>Família</h1>
-        {props.children}
+        { childrenWithProps(props.children, props) } 
     </div>
 )
